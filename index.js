a7fe3a31c10530f5ad7b6fa2810253515f8544a5
Sarunas <sar.dovydaitis@hotmail.com>
2021-07-14 19:13:56 +0300

    sarasas

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
+// norint terminale iskviest si faila;
+// rasome node . (be kabuciu su tarpu)
+
 const Sarasas = require('./Sarasas');
 
-const s = new Sarasas();
+const automobiliai = new Sarasas();  //inicijuojame sarasa
 
-console.log(s);
+automobiliai.prideti('audi');
+automobiliai.prideti('volvo');
+automobiliai.prideti('bmw');
+automobiliai.prideti('nissan');
+automobiliai.prideti('ww');
+automobiliai.prideti('peugeot');
+automobiliai.prideti('range rover');
+
+
+
+automobiliai.spausdinti();
+
+// koki automobili esanti sarase norim redaguoti ir i ka pakeisti
+
+automobiliai.redaguoti(3, 'opel');
+
+automobiliai.pasalinti(1);
+automobiliai.pasalinti(2);
+automobiliai.pasalinti(3);
+
+
+automobiliai.spausdinti();
+
+
+//Sarasas:
+// ================
+// 1. audi
+// 2. volvo
+// ================
